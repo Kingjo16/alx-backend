d67bec3619e7d2684c470f793b7fab212c2f43a4
kingjo <kidusyohannesred@gmail.com>
2024-07-22 15:28:40 -0700

    new fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,8 +31,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get a page from the dataset."""
-        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
+        assert isinstance(page, int) and page > 0, \
+            "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "page_size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
